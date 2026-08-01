e82cc9e52aa923d264716f69b8809b870cb01e18
LOUIS NKAN ENOBONG <98494528+louisnkan@users.noreply.github.com>
2026-08-01 11:37:13 +0100

    Register service worker on page load
    feat: register service worker in layout

--- app/layout.js
+++ app/layout.js
@@ -55,6 +55,11 @@ export default function RootLayout({ children }) {
         <script
           dangerouslySetInnerHTML={{
             __html: `
+            if ('serviceWorker' in navigator) {
+                window.addEventListener('load', function() {
+                  navigator.serviceWorker.register('/sw.js')
+                })
+              }
               document.addEventListener('contextmenu',
                 e => e.preventDefault());
               document.addEventListener('keydown', function(e) {
